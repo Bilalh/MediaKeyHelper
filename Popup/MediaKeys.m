a2efb65ac9566330f11507cb63017ebb2aa95432
Bilal Syed Hussain <bilalshussain@gmail.com>
2012-12-30 01:24:45 +0000

    Added more checks for mplayer

--- Popup/MediaKeys.m
+++ Popup/MediaKeys.m
@@ -24,38 +24,12 @@
 
 }
 
-// from http://collinhenderson.com/post/23804377536/cocoa-snippet-check-if-process-is-running
-+(BOOL)processIsRunning:(NSString *)aProcess{
-    NSTask* task = [[NSTask alloc] init];
-    [task setLaunchPath: @"/usr/bin/top"];
-    
-    NSArray* arguments = [NSArray arrayWithObjects: @"-s", @"1",@"-l",@"3600",@"-stats",@"pid,cpu,time,command", nil];
-    
-    [task setArguments: arguments];
-    
-    NSPipe* pipe = [NSPipe pipe];
-    [task setStandardOutput: pipe];
-    
-    [task setStandardInput:[NSPipe pipe]];
-    
-    [task launch];
-    
-    NSData* processData = [[[task standardOutput]fileHandleForReading]availableData];
-    NSString* processes = [[NSString alloc]initWithData:processData encoding:NSUTF8StringEncoding];
-    if ([processes rangeOfString:aProcess].location != NSNotFound) {
-        return TRUE;
-    }
-    else {
-        return FALSE;
-    }
-}
-
 + (void) next
 {
     NSString *s;
     if ([self isFluidAppRunning] ){
         s =@"osascript -e \'activate application \"Anime\"\' -e \'tell application \"System Events\"\' -e \'\' -e \'set boundss to \"\"\' -e \'tell application \"Anime\"\' -e \'set boundss to get bounds of window 1\' -e \'end tell\' -e \'\' -e \'tell process \"Anime\"\' -e \'set com to \"/usr/local/bin/cliclick -r \" & (get (item 1 of boundss) + 38) & \" \" & (get (item 4 of boundss) - 15)\' -e \'do shell script com\' -e \'\' -e \'keystroke \"h\" using {command down}\' -e \'end tell\' -e \'\' -e \'end tell\'";
-    }else if ([self isMPlayerRunning]){
+    }else if ([self isMPlayerRunning] || [self processIsRunning:@"mplayer2"]){
         s = @"echo 'pt_step 1' > ~/.mplayer/pipe";
     }
     
@@ -67,7 +41,7 @@
     NSString *s;
     if ([self isFluidAppRunning]){
         s = @"osascript -e \'activate application \"Anime\"\' -e \'tell application \"System Events\"\' -e \'\' -e \'set boundss to \"\"\' -e \'tell application \"Anime\"\' -e \'set boundss to get bounds of window 1\' -e \'end tell\' -e \'\' -e \'tell process \"Anime\"\' -e \'set com to \"/usr/local/bin/cliclick \" & (get (item 1 of boundss) + 15) & \" \" & (get (item 4 of boundss) - 15)\' -e \'do shell script com\' -e \'end tell\' -e \'\' -e \'\' -e \'\' -e \'end tell\'";
-    }else if ([self isMPlayerRunning]){
+    }else if ([self isMPlayerRunning] || [self processIsRunning:@"mplayer2"]){
         s = @"echo 'pt_step -1' > ~/.mplayer/pipe";
     }
     
@@ -95,6 +69,32 @@
         return false;
     }
     
+}
+
+// from http://collinhenderson.com/post/23804377536/cocoa-snippet-check-if-process-is-running
++(BOOL)processIsRunning:(NSString *)aProcess{
+    NSTask* task = [[NSTask alloc] init];
+    [task setLaunchPath: @"/usr/bin/top"];
+    
+    NSArray* arguments = [NSArray arrayWithObjects: @"-s", @"1",@"-l",@"3600",@"-stats",@"pid,cpu,time,command", nil];
+    
+    [task setArguments: arguments];
+    
+    NSPipe* pipe = [NSPipe pipe];
+    [task setStandardOutput: pipe];
+    
+    [task setStandardInput:[NSPipe pipe]];
+    
+    [task launch];
+    
+    NSData* processData = [[[task standardOutput]fileHandleForReading]availableData];
+    NSString* processes = [[NSString alloc]initWithData:processData encoding:NSUTF8StringEncoding];
+    if ([processes rangeOfString:aProcess].location != NSNotFound) {
+        return TRUE;
+    }
+    else {
+        return FALSE;
+    }
 }
 
 @end
